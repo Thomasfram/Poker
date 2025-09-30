514862bf06b75be35d86f32b14120eb52d057ce3
Thomas <thomas.framery@2021.icam.fr>
2025-09-30 22:29:13 +0200

    new crop type

--- crop.py
+++ crop.py
@@ -9,11 +9,11 @@ src_dir = Path("ranges")  # dossier racine
 
 def crop_image(path):
     img = Image.open(path)
-
-    cropped_image = img.crop((0, 0, 510, 510))
-    cropped_image.save(path)  # écrase l’original
+    if img.size != (510, 510):
+        cropped_image = img.crop((75, 445, 585, 955))
+        print("Cropped:", p)
+        cropped_image.save(path)  # écrase l’original
 
 
 for p in src_dir.rglob("*.png"):
     crop_image(p)
-    print("Cropped:", p)
